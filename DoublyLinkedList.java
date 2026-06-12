3d344a110c7be35ac8947b4a6aa6e8f6ce45ddb8
Faheem <faheemahamed.mfa1@gmail.com>
2026-06-12 09:42:59 +0530

    Implement Doubly Linked List with insert and search

---
+++ DoublyLinkedList.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+class Node{
+    int data;
+    Node prev;
+    Node next;
+    Node(int data){
+        this.data=data;
+    }
+}
+public class DoublyLinkedList {
+    Node head;
+    void insert(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null)
+            temp=temp.next;
+        temp.next=newNode;
+        newNode.prev=temp;
+    }
+    void display(){
+        Node temp=head;
+
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    void search(int key){
+        Node temp=head;
+        int pos=0;
+        while(temp!=null){
+            if(temp.data==key){
+                System.out.println("Found at position "+pos);
+                return;
+            }
+            temp=temp.next;
+            pos++;
+        }
+        System.out.println("Not Found");
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        DoublyLinkedList list=new DoublyLinkedList();
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+            list.insert(sc.nextInt());
+        list.display();
+        int key=sc.nextInt();
+        list.search(key);
+    }
+}
